d1e5a79795a820c2a7386b13fa72c1c5afd01bfc
shivaiitb21 <62739431+shivaiitb21@users.noreply.github.com>
2020-05-10 16:21:48 +0530

    CASE, END, SUM(CASE)

---
+++ 14_CASE.sql
@@ -0,0 +1,53 @@
+-- Case 
+-- General Statement
+SELECT customer_id, 
+CASE
+	WHEN (customer_id <= 100) THEN 'Premium'
+	WHEN (customer_id BETWEEN 100 AND 200) THEN 'Plus'
+	ELSE 'Normal'
+END AS customer_class
+FROM customer;
+
+-- Case as a Expression
+SELECT customer_id,
+CASE customer_id
+	WHEN 2 THEN 'Winner'
+	WHEN 5 THEN 'Second Place'
+	ELSE 'Normal'
+END AS raffle_results
+FROM customer;
+	
+-- Advanced CASE use
+SELECT 
+SUM(CASE rental_rate
+   	WHEN 0.99 THEN 1
+	ELSE 0
+END) AS bargains,
+SUM(CASE rental_rate
+   	WHEN 2.99 THEN 1
+	ELSE 0   
+END) AS regular,
+SUM(CASE rental_rate
+   	WHEN 4.99 THEN 1
+	ELSE 0
+END) AS premium
+FROM film;
+
+
+-- Challenge
+SELECT DISTINCT(rating) FROM film;
+
+SELECT 
+SUM(CASE rating
+	WHEN 'R' THEN 1
+	ELSE 0
+END) AS r,
+SUM(CASE rating 
+   	WHEN 'PG' THEN 1
+	ELSE 0
+END) AS pg,
+SUM(CASE rating
+   	WHEN 'PG-13' THEN 1
+   	ELSE 0
+END) AS pg13
+FROM film;
